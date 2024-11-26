07ba611637769b040f3b8ec989616dcd480ba760
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-11-26 03:59:45 +0330

    Add project files.

---
+++ ImageCompare/Program.cs
@@ -0,0 +1,100 @@
+﻿using Codeuctivity.ImageSharpCompare;
+using CoenM.ImageHash;
+using CoenM.ImageHash.HashAlgorithms;
+using Shipwreck.Phash;
+using Shipwreck.Phash.Bitmaps;
+using System.Drawing;
+
+var solutionDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory[..AppContext.BaseDirectory.IndexOf("bin")], ".."));
+var img1 = Path.Combine(solutionDir, "img1.png");
+var img2 = Path.Combine(solutionDir, "img2.webp");
+var img11 = Path.Combine(solutionDir, "img11.png");
+
+#region ImageHash
+//https://github.com/coenm/ImageHash
+Console.WriteLine("============================== CoenM.ImageSharp.ImageHash ==============================");
+var averageHash1 = GetAverageHash(img1);
+var averageHash2 = GetAverageHash(img2);
+var averageSimilarity = CompareHash.Similarity(averageHash1, averageHash2);
+Console.WriteLine($"AverageHash Similarity: {averageSimilarity}");
+
+var perceptualHash1 = GetPerceptualHash(img1);
+var perceptualHash2 = GetPerceptualHash(img2);
+var perceptualSimilarity = CompareHash.Similarity(perceptualHash1, perceptualHash2);
+Console.WriteLine($"PerceptualHash Similarity: {perceptualSimilarity}");
+
+var differenceHash1 = GetDifferenceHash(img1);
+var differenceHash2 = GetDifferenceHash(img2);
+var differenceSimilarity = CompareHash.Similarity(differenceHash1, differenceHash2);
+Console.WriteLine($"DifferenceHash Similarity: {differenceSimilarity}");
+#endregion
+
+Console.WriteLine();
+Console.WriteLine();
+
+#region PHash
+//https://github.com/pgrho/phash
+Console.WriteLine("============================== Shipwreck.Phash ==============================");
+var hash1 = ComputeHash(img1);
+var hash2 = ComputeHash(img11);
+var score = ImagePhash.GetCrossCorrelation(hash1, hash2);
+Console.WriteLine($"Cross Correlation: {score * 100}");
+#endregion
+
+Console.WriteLine();
+Console.WriteLine();
+
+#region ImageSharp.Compare
+//https://github.com/Codeuctivity/ImageSharp.Compare
+Console.WriteLine("============================== ImageSharp.Compare ==============================");
+
+var calc0 = Path.Combine(solutionDir, "calc0.jpg");
+var calc1 = Path.Combine(solutionDir, "calc1.jpg");
+
+var calcDiff = ImageSharpCompare.CalcDiff(calc0, calc1); //Must be same size
+
+Console.WriteLine($"PixelErrorCount: {calcDiff.PixelErrorCount}");
+Console.WriteLine($"PixelErrorPercentage: {calcDiff.PixelErrorPercentage}");
+Console.WriteLine($"AbsoluteError: {calcDiff.AbsoluteError}");
+Console.WriteLine($"MeanError: {calcDiff.MeanError}");
+
+using var fileStreamDifferenceMask = File.Create(Path.Combine(solutionDir, "cacl_difference_mask.png"));
+using var maskImage = ImageSharpCompare.CalcDiffMaskImage(calc0, calc1);
+await SixLabors.ImageSharp.ImageExtensions.SaveAsPngAsync(maskImage, fileStreamDifferenceMask);
+#endregion
+
+//var a1 = @"C:\Users\mjebrahimi\Desktop\Temp\Verify-Playground\Tests\_snapshots\TestClass1.Playwright_Page.received.png";
+//var a2 = @"C:\Users\mjebrahimi\Desktop\Temp\Verify-Playground\Tests\_snapshots\TestClass1.Playwright_Page.verified.png";
+//using var xxx = File.Create(Path.Combine(solutionDir, "xxx.png"));
+//using var aaa = ImageSharpCompare.CalcDiffMaskImage(a1, a2);
+//await SixLabors.ImageSharp.ImageExtensions.SaveAsPngAsync(aaa, xxx);
+
+#region Methods
+static ulong GetAverageHash(string filename)
+{
+    var hashAlgorithm = new AverageHash();
+    using var stream = File.OpenRead(filename);
+    return hashAlgorithm.Hash(stream);
+}
+
+static ulong GetDifferenceHash(string filename)
+{
+    var hashAlgorithm = new DifferenceHash();
+    using var stream = File.OpenRead(filename);
+    return hashAlgorithm.Hash(stream);
+}
+
+static ulong GetPerceptualHash(string filename)
+{
+    var hashAlgorithm = new PerceptualHash();
+    using var stream = File.OpenRead(filename);
+    return hashAlgorithm.Hash(stream);
+}
+
+static Digest ComputeHash(string filename)
+{
+    //Windows Only because of using System.Drawing.Common package
+    var bitmap = (Bitmap)Image.FromFile(filename);
+    return ImagePhash.ComputeDigest(bitmap.ToLuminanceImage());
+}
+#endregion
---
+++ Tests/Test1.cs
@@ -0,0 +1,353 @@
+﻿using Microsoft.Playwright;
+using System.Diagnostics.CodeAnalysis;
+
+//[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows")]
+namespace Tests;
+
+[TestClass]
+[UsesVerify]
+public partial class TestClass1 //: VerifyBase
+{
+    [Ignore]
+    [TestMethod]
+    public async Task ImageComparison()
+    {
+        const string filename = "sample.1.png"; //Phash Only works with .png files
+
+        await VerifyFile(filename);
+        //await Verify(File.OpenRead(filename));
+        //await Verify(File.OpenRead(filename), "jpg");
+        //await Verify(File.ReadAllBytes(filename), "jpg");
+
+        #region ImageHash
+        //await VerifyFile(filename)
+        //    //new PerceptualHash() //new AverageHash() //new DifferenceHash()
+        //    .UseImageHash(100, algorithm: new CoenM.ImageHash.HashAlgorithms.PerceptualHash());
+
+        //await ThrowsTask(async () =>
+        //{
+        //    await VerifyFile(filename).UseMethodName("FailInner"); //must be unique (different from the method name)
+        //})
+        ////.DisableDiff() //Do not launch vs code for text diff
+        //.IgnoreStackTrace()
+        //.ScrubLinesContaining("clipboard", "DiffEngineTray"); //Removes any lines that contains that string
+        #endregion
+
+        #region Phash
+        //await VerifyFile(filename)
+        //    .PhashCompareSettings(
+        //        threshold: .8f,
+        //        sigma: 4f,
+        //        gamma: 2f,
+        //        angles: 170);
+        #endregion
+
+        #region ImageSharp.Compare
+        //await VerifyFile(filename).UseImageHash(5); //Images must be the same size
+        #endregion
+
+        #region ImageSharp
+        //await VerifyFile(filename).DisableDiff().EncodeAsJpeg();
+        #endregion
+
+        #region ImageMagick
+        //await VerifyFile(filename)
+        //.ImageMagickComparer(0.001, ImageMagick.ErrorMetric.PerceptualHash)
+        //.ImageMagickBackground(ImageMagick.MagickColor.FromRgb(255, 255, 255))
+        //;
+        #endregion
+    }
+
+    [TestMethod]
+    public async Task CompareComplexObject()
+    {
+        //Default: Compare JSON texts (sensitive about property position!)
+        //Verify.Quibble: Compare JSON property values (NOT sensitive about property position)
+
+        var complexObject = new
+        {
+            Name = "Foo",
+            Age = 25,
+            Address = new
+            {
+                Street = "123 Main St",
+                City = "Redmond",
+                State = "WA",
+                Zip = 98052
+            }
+        };
+
+        await Verify(complexObject);
+    }
+
+
+    static IPlaywright PlaywrightInstance = null!;
+    static IBrowser Browser = null!;
+    IPage Page = null!;
+
+    //[TestInitialize]
+    public async Task InitializePlaywright()
+    {
+        // wait for target server to start
+        //await SocketWaiter.Wait(port: 5000);
+
+        PlaywrightInstance ??= await Playwright.CreateAsync();
+        Browser ??= await PlaywrightInstance.Chromium.LaunchAsync(new() { /*Headless = false*/ });
+        Page = await Browser.NewPageAsync();
+        await Page.GotoAsync("http://localhost:5000");
+        await Assertions.Expect(Page).ToHaveTitleAsync("Home");
+    }
+
+    [TestMethod]
+    public async Task Playwright_Page()
+    {
+        await InitializePlaywright();
+
+        //Page test
+        await Verify(Page); //.DisableDiff()
+
+        //await Verify(Page)
+        //    .PageScreenshotOptions(new() { Quality = 50, Type = ScreenshotType.Jpeg }); //.DisableDiff()
+    }
+
+    [TestMethod]
+    public async Task Playwright_LocatorElement()
+    {
+        await InitializePlaywright();
+
+        var element = Page.Locator("#app > div > div > div.collapse.nav-scrollable");
+
+        //Element test using ILocator
+        await Verify(element);
+
+        //await Verify(element)
+        //    .LocatorScreenshotOptions(new() { Quality = 50, Type = ScreenshotType.Jpeg });
+    }
+
+    [Ignore]
+    [TestMethod]
+    public async Task Playwright_SelectorElement()
+    {
+        await InitializePlaywright();
+
+        var element = await Page.QuerySelectorAsync("#app > div > div > div.collapse.nav-scrollable");
+
+        //Element test
+        await Verify(element);
+
+        //await Verify(element)
+        //    .ElementScreenshotOptions(new() { Quality = 50, Type = ScreenshotType.Jpeg });
+    }
+}
+
+public static class ModuleInitializer
+{
+    public static readonly string SolutionDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory[..AppContext.BaseDirectory.IndexOf("bin")], ".."));
+    public static readonly string SnapshotDir = Path.Combine(SolutionDir, "Tests", "_snapshots");
+
+    [System.Runtime.CompilerServices.ModuleInitializer]
+    public static void Init()
+    {
+        //Compare similarity
+        //https://github.com/VerifyTests/Verify.Phash //NOT Good (only windows)
+        //VerifyPhash.Initialize();
+
+        //Compare similarity
+        //https://github.com/VerifyTests/Verify.ImageHash //Good
+        VerifyImageHash.Initialize();
+
+        //Compare similarity
+        //https://github.com/VerifyTests/Verify.ImageSharp.Compare (
+        //VerifyImageSharpCompare.Initialize(); //Images must be the same size
+
+        //Compare pixel by pixel and similarity (not good but better in pixel by pixel)
+        //https://github.com/VerifyTests/Verify.ImageMagick
+        //VerifyImageMagick.Initialize();
+
+        //Compare pixel by pixel
+        //https://github.com/VerifyTests/Verify.ImageSharp //NOT Good
+        //VerifyImageSharp.Initialize();
+
+        //Compares rendered html and screenshot pixel by pixel (maybe there is a way to disable html comparison) (fails on CI/CD, needs to be run on the same OS or use a custom comparer)
+        //https://github.com/VerifyTests/Verify.HeadlessBrowsers
+        VerifyPlaywright.Initialize(installPlaywright: true);
+        //OS specific rendering
+        //The rendering can very slightly between different OS versions.This can make verification on different machines(eg CI) problematic.
+        //A [custom](https://github.com/VerifyTests/Verify/blob/main/docs/comparer.md) comparer can to mitigate this.
+
+        //Load all plugins (Verify.*.dll)
+        //VerifierSettings.InitializePlugins();
+
+        //Compares JSON property values (NOT sensitive to property positions)
+        //VerifierSettings.UseStrictJson();
+        //VerifyQuibble.Initialize();
+
+        DerivePathInfo((_, _, type, method) => new PathInfo(SnapshotDir, type.Name, method.Name));
+    }
+}
+
+public static class VerifyPlaywright
+{
+    public static bool Initialized { get; private set; }
+
+    public static void Initialize(bool installPlaywright = false)
+    {
+        if (Initialized)
+            throw new("Already Initialized");
+
+        Initialized = true;
+
+        InnerVerifier.ThrowIfVerifyHasBeenRun();
+
+        if (installPlaywright)
+            Program.Main(["install"]);
+
+        VerifierSettings.RegisterFileConverter<IPage>(PageToImage);
+        VerifierSettings.RegisterFileConverter<IElementHandle>(ElementToImage);
+        VerifierSettings.RegisterFileConverter<ILocator>(LocatorToImage);
+    }
+
+    #region Converters
+    static async Task<ConversionResult> PageToImage(IPage page, IReadOnlyDictionary<string, object> context)
+    {
+        await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+        byte[] bytes;
+        var imageType = "png";
+        if (context.GetPageScreenshotOptions(out var options))
+        {
+            bytes = await page.ScreenshotAsync(options);
+            if (options.Type == ScreenshotType.Jpeg)
+                imageType = "jpg";
+        }
+        else
+        {
+            bytes = await page.ScreenshotAsync(new() { FullPage = true, Type = ScreenshotType.Png });
+        }
+
+        return new(
+            null,
+            [new(imageType, new MemoryStream(bytes))]
+        );
+    }
+
+    static async Task<ConversionResult> ElementToImage(IElementHandle element, IReadOnlyDictionary<string, object> context)
+    {
+        byte[] bytes;
+        var imageType = "png";
+        if (context.GetElementScreenshotOptions(out var options))
+        {
+            bytes = await element.ScreenshotAsync(options);
+            if (options.Type == ScreenshotType.Jpeg)
+                imageType = "jpg";
+        }
+        else
+        {
+            bytes = await element.ScreenshotAsync(new() { Type = ScreenshotType.Png });
+        }
+
+        return new(
+            null,
+            [new(imageType, new MemoryStream(bytes))]
+        );
+    }
+
+    static async Task<ConversionResult> LocatorToImage(ILocator locator, IReadOnlyDictionary<string, object> context)
+    {
+        byte[] bytes;
+        var imageType = "png";
+        if (context.GetLocatorScreenshotOptions(out var options))
+        {
+            bytes = await locator.ScreenshotAsync(options);
+            if (options.Type == ScreenshotType.Jpeg)
+                imageType = "jpg";
+        }
+        else
+        {
+            bytes = await locator.ScreenshotAsync(new() { Type = ScreenshotType.Png });
+        }
+
+        return new(
+            null,
+            [new(imageType, new MemoryStream(bytes))]);
+    }
+    #endregion
+
+    #region Set Options
+    public static void PageScreenshotOptions(this VerifySettings settings, PageScreenshotOptions options) =>
+        settings.Context["Playwright.PageScreenshotOptions"] = options;
+
+    public static SettingsTask PageScreenshotOptions(this SettingsTask settings, PageScreenshotOptions options)
+    {
+        settings.CurrentSettings.PageScreenshotOptions(options);
+        return settings;
+    }
+
+    public static void ElementScreenshotOptions(this VerifySettings settings, ElementHandleScreenshotOptions options) =>
+        settings.Context["Playwright.ElementScreenshotOptions"] = options;
+
+    public static SettingsTask ElementScreenshotOptions(this SettingsTask settings, ElementHandleScreenshotOptions options)
+    {
+        settings.CurrentSettings.ElementScreenshotOptions(options);
+        return settings;
+    }
+
+    public static void LocatorScreenshotOptions(this VerifySettings settings, LocatorScreenshotOptions options) =>
+        settings.Context["Playwright.LocatorScreenshotOptions"] = options;
+
+    public static SettingsTask LocatorScreenshotOptions(this SettingsTask settings, LocatorScreenshotOptions options)
+    {
+        settings.CurrentSettings.LocatorScreenshotOptions(options);
+        return settings;
+    }
+    #endregion
+
+    #region Get Options
+    private static bool GetPageScreenshotOptions(this IReadOnlyDictionary<string, object> context, [NotNullWhen(true)] out PageScreenshotOptions? options)
+    {
+        if (context.TryGetValue("Playwright.PageScreenshotOptions", out var value))
+        {
+            options = (PageScreenshotOptions)value;
+            ValidateNoPath(options.Path);
+            return true;
+        }
+
+        options = null;
+        return false;
+    }
+
+    private static bool GetElementScreenshotOptions(this IReadOnlyDictionary<string, object> context, [NotNullWhen(true)] out ElementHandleScreenshotOptions? options)
+    {
+        if (context.TryGetValue("Playwright.ElementScreenshotOptions", out var value))
+        {
+            options = (ElementHandleScreenshotOptions)value;
+            ValidateNoPath(options.Path);
+            return true;
+        }
+
+        options = null;
+        return false;
+    }
+
+    private static bool GetLocatorScreenshotOptions(this IReadOnlyDictionary<string, object> context, [NotNullWhen(true)] out LocatorScreenshotOptions? options)
+    {
+        if (context.TryGetValue("Playwright.LocatorScreenshotOptions", out var value))
+        {
+            options = (LocatorScreenshotOptions)value;
+            ValidateNoPath(options.Path);
+            return true;
+        }
+
+        options = null;
+        return false;
+    }
+    #endregion
+
+    private static void ValidateNoPath(string? path)
+    {
+        if (path != null)
+        {
+            throw new("ScreenshotOptions Path not supported.");
+        }
+    }
+}
